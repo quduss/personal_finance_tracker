feb0f4f82f8866a385bd3847536271fe012ad4ba
quduss <shuaibqudus33@gmail.com>
2024-09-30 17:42:10 +0100

    Create database tables

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ db.init_app(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-from models import User
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -23,8 +21,7 @@ def load_user(user_id):
 def home():
     return "Welcome to Personal Finance Tracker!"
 
-@app.before_first_request
-def create_tables():
+with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
