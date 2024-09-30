c9b699ef06b42f99800a764888112efc1696c9bb
quduss <shuaibqudus33@gmail.com>
2024-09-30 17:38:16 +0100

    Initialize database connection with Flask application instance

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from models import db, User
 
 app = Flask(__name__)
 
@@ -8,7 +8,8 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finance.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
