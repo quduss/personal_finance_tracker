7c32f822ae8e420fbbb2e0fce4eea75353f1c3fa
quduss <shuaibqudus33@gmail.com>
2024-10-03 12:29:48 +0100

    Replace deprecated Query.get() with Session.get()

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ login_manager.login_view = 'login'
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return db.session.get(User, int(user_id))
 
 @app.route('/')
 def home():
