c46dab8058e5386c8007fe55b740b4c95c2953bf
quduss <shuaibqudus33@gmail.com>
2024-10-05 20:03:44 +0100

    Make transaction_type an enum

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from sqlalchemy import Enum
+import enum
 
 db = SQLAlchemy()
 
@@ -12,13 +14,23 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)
 
+class TransactionTypeEnum(enum.Enum):
+    INCOME = "Income"
+    EXPENSE = "Expense"
+
 class Transaction(db.Model):
     __tablename__ = 'transactions'
 
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=True)
     amount = db.Column(db.Float, nullable=False)
-    transaction_type = db.Column(db.String(10), nullable=False)
+    transaction_type = db.Column(Enum(TransactionTypeEnum), nullable=False)
     category = db.Column(db.String(50), nullable=False)
     date = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    # Relationship to User
+    user = db.relationship('User', backref=db.backref('transactions', lazy=True))
+
+    def __repr__(self):
+        return f"<Transaction {self.transaction_type}: {self.amount} - {self.description} (Category: {self.category})>"
