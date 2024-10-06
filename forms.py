8fa89b24e5141fb0b5df1e92bbfb178913dafe9f
quduss <shuaibqudus33@gmail.com>
2024-10-06 12:25:01 +0100

    Create form for a transaction

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired
+from wtforms import StringField, PasswordField, SubmitField, FloatField, RadioField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired, NumberRange
+from models import TransactionTypeEnum
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
@@ -13,3 +14,10 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class TransactionForm(FlaskForm):
+    amount = FloatField('Amount', validators=[DataRequired(), NumberRange(min=0.01, message="Amount must be greater than 0")])
+    description = StringField('Description', validators=[DataRequired()])
+    transaction_type = RadioField('Transaction Type', choices=[(TransactionTypeEnum.INCOME.value, 'Income'), (TransactionTypeEnum.EXPENSE.value, 'Expense')], validators=[DataRequired()])
+    category = StringField('Category', validators=[DataRequired()])
+    submit = SubmitField('Add Transaction')
