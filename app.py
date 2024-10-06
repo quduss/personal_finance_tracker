4a1361fc6eb8d747e65fbfcd3172fd0129460c38
quduss <shuaibqudus33@gmail.com>
2024-10-06 12:07:03 +0100

    Add flash message for logout()

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
+    flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
 
 with app.app_context():
